a794cdf43290061c66fba33df63b0a778015d7ec
omnia alessawy <aminaamohamedd1@gmail.com>
2024-08-20 04:34:12 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").style.color = "rgb(255,250,255)";
+document.getElementById("demo").style.backgroundColor="red";
+document.getElementById("demo").style.textAlign="center"; 
